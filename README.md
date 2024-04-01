6bd9a10a09cf0c240007de0d29d1fadf926211ee
Thienan1092005 <ogyminecraft497@gmail.com>
2024-04-01 17:49:51 +0700

    new page

--- README.md
+++
@@ -1,3 +0,0 @@
-# profile
-
-đây là cái web lỏ của Yuki đựa trên lfie gốc của takeshi và mizu ❤️
---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const listItems = document.querySelectorAll(".item");
+  const rightCtns = document.querySelectorAll(".right--ctn");
+
+  // Mặc định mở mục "2020"
+  listItems[0].style.color = "#0cf";
+  rightCtns[0].classList.add("active");
+
+  listItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      // Bỏ lớp active khỏi tất cả các rightCtns
+      rightCtns.forEach((rightCtn) => {
+        rightCtn.classList.remove("active");
+      });
+
+      // Thêm lớp active vào rightCtn tương ứng với mục được chọn
+      rightCtns[index].classList.add("active");
+
+      // Set màu cho tất cả các mục về màu trắng
+      listItems.forEach((item) => {
+        item.style.color = "white";
+      });
+
+      // Set màu cho mục được chọn
+      item.style.color = "#0cf";
+    });
+  });
+});
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+btn1.addEventListener("click", () => {
+  window.open("https://www.facebook.com/yukicute1009", "_blank", "noopener");
+});
